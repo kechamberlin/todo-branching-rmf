157974d2436bb28c5bb88c160e075f2f94d580e6
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-07-31 15:39:31 -0700

    imported db, firebase, material ui components, and react hooks

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import db from './firebase-config';
+import firebase from 'firebase';
+import { AddCircleOutlineRounded } from '@material-ui/icons';
+import { Button, TextField, Container } from '@material-ui/core';
+import { useEffect, useState } from 'react';
+
 export default function App() {
   return (
     <div>
