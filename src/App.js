5ca03be307169317f595460346eb8a9fcfeecd2f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-07-31 15:40:35 -0700

    created users, firstName, and lastName states

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import { Button, TextField, Container } from '@material-ui/core';
 import { useEffect, useState } from 'react';
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
   return (
     <div>
       <h1>Start here!</h1>
