f63d84b8b0e94df9e633406f577418c3812f2fa6
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2021-07-31 12:51:36 -0700

    deleted boilerplate

--- src/App.js
+++ src/App.js
@@ -7,17 +7,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>This is Feature A added by Developer A</h1>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
         <p>This is Feature B added by Developer B</p>
         <p>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat,
